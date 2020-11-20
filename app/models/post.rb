6ccff3b9e4f452f71c743240c60495e3e67bace8
EC2 Default User <ec2-user@ip-172-31-16-122.ec2.internal>
2020-11-20 08:13:59 +0000

    [Add] favorites

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,15 @@ class Post < ApplicationRecord
   belongs_to :user
   attachment :image
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :image, presence: true
 	with_options presence: true do
     validates :sentence
     validates :meaning
     validates :explanation
+  end
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :image
   validates :name, presence: true, length: {minimum: 2, maximum: 20}
   validates :profile, length: {maximum: 100}
+
 end
