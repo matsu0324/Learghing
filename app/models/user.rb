1cbced0f33efaef4a084029a3dc6ef5e2ee86875
EC2 Default User <ec2-user@ip-172-31-16-122.ec2.internal>
2020-12-06 04:39:17 +0000

    [Add] いいねした投稿一覧

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_posts, through: :favorites, source: :post
   has_many :sns_credential, dependent: :destroy
   has_many :events
   attachment :image
