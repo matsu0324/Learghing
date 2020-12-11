9b918c44e4287543a62c37fd8a88eb3b1999c0d7
EC2 Default User <ec2-user@ip-172-31-16-122.ec2.internal>
2020-12-11 05:11:45 +0000

    [Fix] レイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    flash[:notice] = "Signed in successfully."
+    flash[:notice] = "ログインに成功しました。"
     user_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
-    flash[:notice] = "Signed out successfully."
+    flash[:notice] = "ログアウトに成功しました。"
     root_path
   end
 
