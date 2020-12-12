38466fae8a3c39341a93b7131ea3c1853018d235
EC2 Default User <ec2-user@ip-172-31-16-122.ec2.internal>
2020-12-12 10:52:19 +0000

    [Fix] バリデーション

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def create
     @event = Event.new(event_params)
     @event.save!
@@ -13,6 +16,7 @@ class EventsController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
+    #@user = current_user
     event = Event.find(params[:id])
     event.destroy
     redirect_to user_path(@user)
@@ -22,5 +26,12 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:title, :start, :end, :user_id, :body)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
